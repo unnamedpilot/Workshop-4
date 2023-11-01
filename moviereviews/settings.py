44366bf304b01c80ee8e39c59adbebd39a9014a8
UnnamedAlt <83482674+UnnamedAlt@users.noreply.github.com>
2023-11-01 10:27:47 -0500

    Actualización de ALLOWED_HOSTS

--- moviereviews/settings.py
+++ moviereviews/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = '2hl5684eyz&6jd+3-rx=vg8enjb+d=f06p^lt^z%%nel1qds_q'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['34.173.100.115']
+ALLOWED_HOSTS = ['34.69.223.184']
 
 
 # Application definition
